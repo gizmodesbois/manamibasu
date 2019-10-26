a5e60b5b251afd836b3f184fd35853987cc92c2e
Simon Henrotte <henrotte.simon@gmail.com>
2019-10-26 17:36:09 +0200

    [MAM-7] Add play kana to guess

--- src/options/app.js
+++ src/options/app.js
@@ -5,6 +5,7 @@ class Settings {
   romajiKana;
   displayTranslation;
   furigana;
+  dropdownKanas;
 
   constructor() {
     this.checkboxes = document.querySelectorAll('input[type="checkbox"]');
@@ -13,19 +14,36 @@ class Settings {
     this.romajiKana = document.getElementById('displayRomaji')
     this.displayTranslation = document.getElementById('displayTranslation')
     this.furigana = document.getElementById('furigana')
+    this.includeMaru = document.getElementById('includeMaru')
+    this.includeTenten = document.getElementById('includeTenten')
+    this.dropdownKanas = document.getElementsByClassName('dropdown-kana');
+    
     this.init();
     this.listenCheckboxes();
+    this.listenKanaChanges();
   }
 
   init() {
     chrome.storage.local.get(null, items => {
-      const { hiragana, katakana, romajiKana, furigana, displayTranslation } = items;
+      const { hiragana, katakana, romajiKana, furigana, displayTranslation, includeMaru, includeTenten } = items;
       this.hiragana.checked = !!hiragana;
       this.katakana.checked = !!katakana;
       this.romajiKana.checked = !!romajiKana;
       this.displayTranslation.checked = !!displayTranslation;
       this.furigana.checked = !!furigana;
+      this.includeMaru.checked = !!includeMaru;
+      this.includeTenten.checked = !!includeTenten;
     });
+  }
+  
+  listenKanaChanges() {
+    for(let i=0; i < this.dropdownKanas.length; i++){
+      this.dropdownKanas[i].addEventListener('change', (e) => {
+        const key = e.target.getAttribute('data-kana')
+        const value = e.target.value
+        this.saveKey(key, value);
+      })
+    }
   }
 
   saveKey(key, value) {
