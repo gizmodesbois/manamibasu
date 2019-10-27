ecffd91a79e2d2f7f0eef9c5df32ce27afd4f9c2
Simon Henrotte <henrotte.simon@gmail.com>
2019-10-27 12:21:00 +0100

    [MAM-8] Handling typography

--- src/override/app.js
+++ src/override/app.js
@@ -1,3 +1,4 @@
+let currentTypography;
 document.querySelector('#go-to-options').addEventListener('click', () => {
   chrome.tabs.create({
     url: chrome.extension.getURL('src/options/index.html')
@@ -10,31 +11,38 @@ document.querySelector('#go-to-options').addEventListener('click', () => {
 })
 
 chrome.storage.local.get(null, items => {
-  const { displayRomaji, furigana, displayTranslation } = items;
+  const { displayRomaji, furigana, displayTranslation, typography } = items;
   const main = document.getElementsByTagName('main')[0]
   if(!displayRomaji) { main.classList.add('hide-romaji') }
   if(!furigana) { main.classList.add('hide-furigana') }
   if(!displayTranslation) { main.classList.add('hide-translation') }
+  if(typography) {
+    document.body.classList.add(typography)
+    currentTypography = typography
+  } 
 });
 
 chrome.storage.onChanged.addListener((changes, namespace) => {
   for (const key in changes) {
     const { newValue } = changes[key];
     const element = document.getElementById(key)
+    const main = document.getElementsByTagName('main')[0]
     if(key === 'romajiKana') {
       newValue ? element.classList.remove('hidden') : element.classList.add('hidden')
     }
     if(key === 'furigana') {
-      const main = document.getElementsByTagName('main')[0]
       newValue ? main.classList.remove('hide-furigana') : main.classList.add('hide-furigana')
     }
     if(key === 'displayRomaji') {
-      const main = document.getElementsByTagName('main')[0]
       newValue ? main.classList.remove('hide-romaji') : main.classList.add('hide-romaji')
     }
     if(key === 'displayTranslation') {
-      const main = document.getElementsByTagName('main')[0]
       newValue ? main.classList.remove('hide-translation') : main.classList.add('hide-translation')
+    }
+    if(key === 'typography') {
+      document.body.classList.remove(currentTypography)
+      document.body.classList.add(newValue)
+      currentTypography = newValue;
     }
   }
 });
